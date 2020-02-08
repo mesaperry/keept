0a219d6cc7acc105d5c687add93c7d9724f25306
Mesa Perry <mesperrus@gmail.com>
2020-02-08 00:22:31 -0600

    fixed home routing

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,42 @@ var router = express.Router();
 var fs = require('fs');
 var mysql = require('mysql');
 const path  = require('path');
-
-
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "ringosucks333",
-	database: "keept"
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
 
-con.connect(function(err) {
-	if (err) throw err;
-	con.query(`SELECT * FROM instances`, function(err, result, fields) {
+rl.question("root password: ", function(input) {
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: input,
+		database: "keept"
+	});
+
+	con.connect(function(err) {
 		if (err) throw err;
-		for (group of result) {
-			router.get('/' + group['group_id'], function(req, res) {
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.write('meme cat');
-				res.end();
-			});
-		}
+		con.query(`SELECT * FROM instances`, function(err, result, fields) {
+			if (err) throw err;
+			for (group of result) {
+				router.get('/' + group['group_id'], function(req, res) {
+					res.writeHead(200, {'Content-Type': 'text/html'});
+					res.write('meme cat');
+					res.end();
+				});
+			}
+		});
+	});
+
+	router.get('/', function(req, res) {
+		res.sendFile('index.html', {
+			root: path.join(__dirname, '../')
+		});
 	});
 });
 
 
-router.get('/', function(req, res) {
-	res.sendFile('static/html/index.html');
-});
-
 
 module.exports = router;
-
-
