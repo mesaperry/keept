34c640e8a42b0fca368dd593584bfe613d9429e7
Mesa Perry <mesperrus@gmail.com>
2020-02-09 23:26:50 -0600

    basic viewing implementation

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pug = require('pug');
 var fs = require('fs');
 const mysql = require('mysql');
 const path  = require('path');
@@ -10,81 +11,113 @@ const rl = readline.createInterface({
 });
 const moment = require('moment');
 
-const workspace = require('./workspace.js');
-
 
 
 // referenced from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function makeid(length) {
-	var result           = '';
-	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	var charactersLength = characters.length;
-	for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 var generate_uid = function(con, callback) {
-	var id = makeid(16);
-	con.query(`SELECT * FROM instances WHERE group_id='${id}'`, function(err, result, fields) {
-		if (err) throw err;
-		if (result.length !== 0) generate_uid(con, callback);
-		else callback(id);
-	});
+    var id = makeid(16);
+    con.query(`SELECT * FROM instances WHERE group_id='${id}'`, function(err, result, fields) {
+        if (err) throw err;
+        if (result.length !== 0) generate_uid(con, callback);
+        else callback(id);
+    });
 };
 
 
 
 rl.question("root password: ", function(input) {
-	var con = mysql.createConnection({
-		host: "localhost",
-		user: "root",
-		password: input,
-		database: "keept"
-	});
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: input,
+        database: "keept"
+    });
 
-	con.connect(function(err) {
-		if (err) throw err;
+    con.connect(function(err) {
+        if (err) throw err;
 
-		function createWorkspace(req, res) {
-			workspace(req, res, con);
-		}
+        var createGroupPage = function(id, res) {
+            con.query(`SELECT * FROM notes WHERE group_id='${id}'`, function(err, result, fields) {
+                if (err) throw err;
+                var note_data = {};
+                // console.log(id);
+                // console.log(result);
 
-		con.query('SELECT * FROM instances', function(err, result, fields) {
-			if (err) throw err;
-			for (group of result) {
-				router.get('/' + group['group_id'], createWorkspace);
-			}
-		});
+                // var organize_data = new Promise((resolve, reject) => {
+                //     result.forEach((comment, i) => {
+                //         if (!(comment['note'] in obj)) {
+                //             note_data.comment['note'] = [comment['description']];
+                //         }
+                //         else {
+                //             note_data.comment['note'].push(comment['description']);
+                //         }
+                //         if (i === results.length - 1) resolve();
+                //     });
+                // });
 
-		router.get('/', function(req, res) {
-			res.sendFile('index.html', {
-				root: path.join(__dirname, '../')
-			});
-		});
+                // organize_data.then(() => {
+                //     console.log(note_data);
+                //     res.render('workspace', { note_data : note_data });
+                // });
+                for (comment of result) {
+                    if (!(comment['note'] in note_data)) {
+                        note_data[comment['note']] = [comment['description']];
+                    }
+                    else {
+                        note_data[comment['note']].push(comment['description']);
+                    }
+                }
+                console.log(note_data);
+                res.render('workspace', { note_data : note_data });
+            });
+        };
 
-		router.post('/workspace', function(req, res) {
-			generate_uid(con, function(id) {
-				// referenced from https://stackoverflow.com/questions/23977548/using-node-js-express-and-mysql-to-insert-for-a-timestamp
-				var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
-				var post = {
-					group_id : id,
-					modified : mysqlTimestamp
-				};
+        con.query('SELECT * FROM instances', function(err, result, fields) {
+            if (err) throw err;
+            for (group of result) {
+                router.get('/' + group['group_id'], function(req, res) {
+                    createGroupPage(group['group_id'], res);
+                });
+            }
+        });
 
-				con.query('INSERT INTO instances SET ?', post, function(err, result) {
-					if (err) throw err;
-    		});
+        router.get('/', function(req, res) {
+            res.render('index');
+        })
 
-    		router.get('/' + id, createWorkspace);
+        router.post('/workspace', function(req, res) {
+            generate_uid(con, function(uid) {
+                // referenced from https://stackoverflow.com/questions/23977548/using-node-js-express-and-mysql-to-insert-for-a-timestamp
+                var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+                var post = {
+                    group_id : uid,
+                    modified : mysqlTimestamp
+                };
 
-				res.redirect('./' + id);
+                con.query('INSERT INTO instances SET ?', post, function(err, result) {
+                    if (err) throw err;
+                });
 
-			});
-		});
+                router.get('/' + uid, function(req, res) {
+                    createGroupPage(uid, res);
+                });
 
-	});
+                res.redirect('./' + uid);
+
+            });
+        });
+
+    });
 });
 
 
