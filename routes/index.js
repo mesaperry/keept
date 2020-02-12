a5ba7efe91c64fb55c017f12ed07524710d9bcd7
Mesa Perry <mesperrus@gmail.com>
2020-02-12 02:57:06 -0600

    reworked mysql connection

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,66 @@ var pug = require('pug');
 var fs = require('fs');
 const mysql = require('mysql');
 const path  = require('path');
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 const moment = require('moment');
 
 
+const lines_in = fs.readFileSync('mysqllogin.txt', 'UTF-8');
+const lines_split = lines_in.split(/\r?\n/);
+
+var mysql_login = {};
+mysql_login.host = lines_split[0];
+mysql_login.user = lines_split[1];
+mysql_login.password = lines_split[2];
+mysql_login.database = lines_split[3];
+
+var pool = mysql.createPool(mysql_login);
+
+getGroupNotes = function(id, callback) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+    var sql = `SELECT * FROM notes WHERE group_id='${id}'`;
+    con.query(sql, callback);
+  });
+};
+
+getInstances = function(callback) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+    var sql = 'SELECT * FROM instances';
+    con.query(sql, callback);
+  });
+};
+
+getInstance = function(id, callback) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+    var sql = `SELECT * FROM instances WHERE group_id='${id}'`;
+    con.query(sql, callback);
+  });
+};
+
+insertInstance = function(post, callback) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+    var sql = 'INSERT INTO instances SET ?';
+    con.query(sql, post, callback);
+  });
+};
+
+insertNote = function(post, callback) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+    var sql = 'INSERT INTO notes SET ?';
+    con.query(sql, post, callback);
+  });
+};
+
+deleteNote = function(delete_query, callback) {
+  pool.getConnection(function(err, con) {
+    if (err) throw err;
+    con.query(delete_query, callback);
+  });
+};
 
 // referenced from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function makeid(length) {
@@ -24,110 +76,96 @@ function makeid(length) {
   return result;
 }
 
-var generate_uid = function(con, callback) {
+var generate_uid = function(callback) {
   var id = makeid(16);
-  con.query(`SELECT * FROM instances WHERE group_id='${id}'`, function(err, result, fields) {
+  getInstance(id, function(err, result, fields) {
     if (err) throw err;
-    if (result.length !== 0) generate_uid(con, callback);
+    if (result.length !== 0) generate_uid(callback);
     else callback(id);
   });
 };
 
 
 
-rl.question("root password: ", function(input) {
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: input,
-    database: "keept"
-  });
 
-  con.connect(function(err) {
+var createGroupPage = function(id, res) {
+  getGroupNotes(id, function(err, result, fields) {
     if (err) throw err;
+    var note_data = {};
+    for (comment of result) {
+      if (!(comment['note'] in note_data)) {
+        note_data[comment['note']] = [comment['description']];
+      }
+      else {
+        note_data[comment['note']].push(comment['description']);
+      }
+    }
+    res.render('workspace', { note_data : note_data });
+  });
+};
 
-    var createGroupPage = function(id, res) {
-      con.query(`SELECT * FROM notes WHERE group_id='${id}'`, function(err, result, fields) {
-        if (err) throw err;
-        var note_data = {};
-        for (comment of result) {
-          if (!(comment['note'] in note_data)) {
-            note_data[comment['note']] = [comment['description']];
-          }
-          else {
-            note_data[comment['note']].push(comment['description']);
-          }
-        }
-        res.render('workspace', { note_data : note_data });
-      });
+getInstances(function(err, result, fields) {
+  if (err) throw err;
+  for (group of result) {
+    router.get('/' + group['group_id'], function(req, res) {
+      createGroupPage(group['group_id'], res);
+    });
+  }
+});
+
+router.get('/', function(req, res) {
+  res.render('index');
+})
+
+router.post('/workspace', function(req, res) {
+  generate_uid(function(uid) {
+    // referenced from https://stackoverflow.com/questions/23977548/using-node-js-express-and-mysql-to-insert-for-a-timestamp
+    var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+    var post = {
+      group_id : uid,
+      modified : mysqlTimestamp
     };
 
-    con.query('SELECT * FROM instances', function(err, result, fields) {
+    insertInstance(post, function(err, result) {
       if (err) throw err;
-      for (group of result) {
-        router.get('/' + group['group_id'], function(req, res) {
-          createGroupPage(group['group_id'], res);
-        });
-      }
     });
 
-    router.get('/', function(req, res) {
-      res.render('index');
-    })
-
-    router.post('/workspace', function(req, res) {
-      generate_uid(con, function(uid) {
-        // referenced from https://stackoverflow.com/questions/23977548/using-node-js-express-and-mysql-to-insert-for-a-timestamp
-        var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
-        var post = {
-          group_id : uid,
-          modified : mysqlTimestamp
-        };
-
-        con.query('INSERT INTO instances SET ?', post, function(err, result) {
-          if (err) throw err;
-        });
-
-        router.get('/' + uid, function(req, res) {
-          createGroupPage(uid, res);
-        });
-
-        res.redirect('./' + uid);
-
-      });
+    router.get('/' + uid, function(req, res) {
+      createGroupPage(uid, res);
     });
 
-    router.post('/newnote', function(req, res) {
-      var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
-      var post = {
-        description : req.body['description'],
-        note : req.body['note'],
-        group_id : req.body['group_id'],
-        modified : mysqlTimestamp
-      }
-
-      con.query('INSERT INTO notes SET ?', post, function(err, result) {
-          if (err) throw err;
-        });
-    });
-
-    router.post('/deletenote', function(req, res) {
-      var delete_query = `
-        DELETE FROM notes
-        WHERE description='${req.body['description']}'
-        AND note='${req.body['note']}'
-        AND group_id='${req.body['group_id']}'
-      `
-
-      console.log(delete_query);
-      con.query(delete_query, function(err, result) {
-          if (err) throw err;
-          console.log(result);
-        });
-    });
+    res.redirect('./' + uid);
 
   });
 });
+
+router.post('/newnote', function(req, res) {
+  var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+  var post = {
+    description : req.body['description'],
+    note : req.body['note'],
+    group_id : req.body['group_id'],
+    modified : mysqlTimestamp
+  }
+
+  insertNote(post, function(err, result) {
+      if (err) throw err;
+    });
+});
+
+router.post('/deletenote', function(req, res) {
+  var delete_query = `
+    DELETE FROM notes
+    WHERE description='${req.body['description']}'
+    AND note='${req.body['note']}'
+    AND group_id='${req.body['group_id']}'
+  `
+
+  deleteNote(delete_query, function(err, result) {
+      if (err) throw err;
+    });
+});
+
 
 
 module.exports = router;
